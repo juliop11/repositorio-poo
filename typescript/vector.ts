5e695fa5c09ff811bdc0da405040d5380ddd5121
juliop11 <julioperezobrero@hotmail.com>
2023-02-10 09:51:38 +0100

    sin terminar

--- typescript/vector.ts
+++ typescript/vector.ts
@@ -1,23 +1,45 @@
-class vector {
+// • Atributo Privado:
 
-    private elements:Number[];
+// - elements: array of numbers.
 
-    constructor(n:number,k:number){
+// • El Constructor debe contener los siguientes parámetros:
 
-        this.elements = n
+// - n: number. Longitud del vector.
+// - k: number. Máximo valor de los elementos del vector.
+// Este constructor tiene que crear el atributo elements formado por n números aleatorios entre 0 y k.
 
-        function crearVector(n, m) {
+class Vector {
 
-            let number = [];
-        
-            for (let i = 0; i < n; i++) {
-        
-                number.push(Math.round(Math.random() * m));
-            }
-        
-            return number;
-        
+    private elements: Number[];
+
+    constructor(n: number, k: number) {
+
+        this.elements = [];
+
+        for (let i = 0; i < n; i++) {
+
+            this.elements.push(Math.round(Math.random() * k))
         }
-        
     }
+    public print() {
+
+        return this.elements;
+    }
+
+    // public add(v1: number[]): number {
+
+    //     let total = 0;
+    //     let array1 = [];
+
+    //     for (let i = 0; i < v1.length; i++) {
+
+    //         total[i] = array1[i] + v1[i]
+    //         return total;
+    //     }
+    // }
 }
+let array: Vector = new Vector(5, 20);
+ console.log(array.print());
+// console.log(array.add([2, 3, 4, 5, 6]));
+
+
